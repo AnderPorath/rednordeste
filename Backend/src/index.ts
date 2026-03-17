e32472c26118b75486048cc7d70b941ed16206ab
Ander Alejandro Porath Ramirez <anderporath@MacBook-Air-de-Ander.local>
2026-03-17 18:12:10 -0300

    add root route

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -31,8 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Ruta de prueba en la raíz
-app.get("/", (_req, res) => {
+app.get("/", (req, res) => {
   res.send("Backend funcionando");
 });
 
